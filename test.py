6fea689d91dd822ab39312343e5ed514aa8e32c0
Ashwin R <ashwingopiram@gmail.com>
2018-08-27 05:07:43 -0700

    Added unit test

--- test.py
+++ test.py
@@ -1,50 +1,40 @@
-import requests
+import unittest
+import doctors
 
-while True:
-    print ( "Choose:\n"
-            "1. Get doctors\n"
-            "2. Add appointment\n"
-            "3. Show appointments \n"
-            "4. Delete appointment\n" )
-    choice = raw_input()
-    if choice == '1':
-        r = requests.get('http://127.0.0.1:5000/doctors')
-        print r.text + '\n'
+data = doctors.DoctorData()
+doc1_id = data.doctors.keys()[0]
 
-    elif choice == '2':
-        print "Input doc_id\n"
-        doc_id = raw_input()
-        url = 'http://127.0.0.1:5000/appointments'
-        payload = ('{"doc_id":%s, \
-                    "date": "09/25/1992", \
-                    "time": "10:30", \
-                    "kind":"New Patient", \
-                    "patient_last_name": "Fieri", \
-                    "patient_first_name":"Guy"}' %doc_id)
-        headers = {'content-type': 'application/json'}
-        r = requests.post(url, data=payload, headers=headers)
-        print r.text + '\n'
+class DoctorDataTest(unittest.TestCase):
+    """Unit test for DoctorData"""
+     
+    def test_get_doctors(self):
+        """Unit test for get_doctors"""
+        self.assertTrue(data.get_doctors())
+     
+    def test_add_appointment(self):
+        """Unit test for add_appointment"""
+        with self.assertRaises(KeyError):
+            data.add_appointment({"doc_id":-100})
 
-    elif choice == '3':
-        print "Input doc_id\n"
-        doc_id = raw_input()
-        url = 'http://127.0.0.1:5000/appointments'
-        payload = ('{"doc_id":%s, \
-                    "date": "09/25/1992"}' %doc_id)
-        headers = {'content-type': 'application/json'}
-        r = requests.get(url, data=payload, headers=headers)
-        print r.text + '\n'
+        with self.assertRaises(KeyError):
+            data.add_appointment({})
 
-    elif choice == '4':
-        print "Input doc_id\n"
-        doc_id = raw_input()
-        print "Input apt_id\n"
-        apt_id = raw_input()
-        url = 'http://127.0.0.1:5000/appointments'
-        payload = ('{"doc_id":%s, \
-                    "date": "09/25/1992", \
-                    "apt_id":%s}' %(doc_id,apt_id))
-        headers = {'content-type': 'application/json'}
-        r = requests.delete(url, data=payload, headers=headers)
-        print r.text + '\n'
+    def test_get_appointments(self):
+        """Unit test for get_appointments"""
+        with self.assertRaises(KeyError):
+            data.get_appointments(-100, "")
 
+        with self.assertRaises(KeyError):
+            data.get_appointments(doc1_id ,"")
+
+    def test_del_appointment(self):
+        """Unit test for el_appointment"""
+        with self.assertRaises(KeyError):
+            data.del_appointment(-100, "", "")
+
+        with self.assertRaises(KeyError):
+            data.del_appointment(doc1_id ,"","")
+
+
+if __name__ == '__main__':
+    unittest.main()
