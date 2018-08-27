ab6e949bbf3ac3823aeaf76677fb628f4522141d
Ashwin R <ashwingopiram@gmail.com>
2018-08-26 20:37:07 -0700

    Add new appointment feature added

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask, url_for
+from flask import Flask
+from flask import url_for
+from flask import json
+from flask import request
 
 import doctors
 
 app = Flask(__name__)
 doc_data = doctors.DoctorData()
 
-@app.route('/doctors')
+@app.route('/doctors', methods = ['GET'])
 def api_doctors():
     return str(doc_data.get_doctors())
+
+@app.route('/appointments', methods = ['GET', 'POST', 'DELETE'])
+def api_appointments():
+	if (request.method == 'POST' and request.headers['Content-Type'] == 'application/json'):
+    		#app.logger.info(request.json)
+    		return doc_data.add_appointment(request.json)
 
 if __name__ == '__main__':
     
     print doc_data.get_doctors()
-    app.run()
+    app.run(debug=True)
--- doctors.py
+++ doctors.py
@@ -9,32 +9,67 @@ class DoctorData(object):
 		'''
 		Adding sample doctors
 		'''
-		self.doctors = dict()
+		self.doctors = dict() # Key: Doc_ID Value: Doctor()
 		self.add_doctor('John', 'Doe')
 		self.add_doctor('Jane', 'Doe')
 
 	def add_doctor(self, first_name, last_name):
-		randid = str(randint(0, 9999))
-		print randid
-		self.doctors[randid] = Doctor(first_name, last_name)
+		doc_id = randint(0, 0)
+		self.doctors[doc_id] = Doctor(first_name, last_name)
 
 	def get_doctors(self):
-		print self.doctors
-		return [(doc.last_name, doc.first_name) for doc in self.doctors.values()]
+		#print self.doctors
+		return [(doc[0], doc[1].last_name, doc[1].first_name) for doc in self.doctors.items()]
 
+	def add_appointment(self, json_data):
+		'''
+		Assuming a valid doctor and a date is already selected in the GUI before requesting a new appointment.
 
+		'''
+		doctor = self.doctors[int(json_data['doc_id'])]
+		date = json_data['date']
+		time = json_data['date']
+		apt_id = randint(1, 9999)
+		apt_data = [json_data["patient_first_name"],
+					json_data["patient_last_name"],
+					json_data["date"],
+					json_data['time'],
+					json_data['kind']]
+		return doctor.add_appointment(date, time, apt_id, apt_data)
+		
 class Doctor(object):
 	"""Data structure to hold doctor data"""
 	def __init__(self, first_name, last_name):
 		self.first_name = first_name
 		self.last_name = last_name
-		self.dates = dict()
+		self.dates = dict() # Key: Date Value: DateInfo()
 
-class DateSlot(object):
-	"""docstring for DateSlot"""
-	def __init__(self, date):
-		self.appointments = Appointment()
-		self.busy = dict()
+	def get_appointments(self):
+		return 
+
+	def add_appointment(self, date, time, apt_id, apt_data):
+		'''
+		Retrieve Date Info for the particular date for the doctor and add appointment 
+		'''
+		# If New date
+		if date not in self.dates:
+			self.dates[date] = DateInfo()
+		date_slot = self.dates[date]
+		return date_slot.add_appointment(time, apt_id, apt_data)
+
+class DateInfo(object):
+	"""docstring for DateInfo"""
+	def __init__(self):
+		self.appointments = dict() # Key: Appointment_ID Value: Appointment()
+		self.booked = dict() # Key: Time Value: True
+
+	def add_appointment(self, time, apt_id, apt_data):
+		if time not in self.booked:
+			self.booked[time] = True
+			self.appointments[apt_id] = Appointment(*apt_data)
+			return "Appointment booked."
+		else:
+			return "Appointment not successful. The slot is already booked."
 
 class Appointment(object):
 	"""docstring for Appointment"""
